4fd5932cbcc2ba3d0fc0f092bb9e65d849707568
bhdrYildiz <bahadiryildiz137@gmail.com>
2023-10-23 17:07:39 +0300

    second commit

---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "043e67dc8amsh10a2c3314014d5dp1100a3jsn77c08cb8cd96",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9b65c8694c6200d32de33ab1d3aa6dbf"
